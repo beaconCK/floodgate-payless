2c5413295dd8c4f5aaebaa36f0d55542fc4bd2ed
Chika <chkonyia@yahoo.com>
2021-12-16 15:01:36 +0000

    Add files via upload
    Kick start...

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'floodgate-payless',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
